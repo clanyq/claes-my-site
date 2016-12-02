ac4c74404d2ad5d9592866690d1dcdbef7919201
Claes Nykvist <claes.nykvist@trell.se>
2016-12-02 14:20:46 +0100

    set up base models for pressapp

--- claesmysite/settings.py
+++ claesmysite/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'claesmysite',
     'compressor',
+    'newsapp',
+    'pressapp',
 ]
 
 MIDDLEWARE = [
--- pressapp/models.py
+++ pressapp/models.py
@@ -1,5 +1,15 @@
 from __future__ import unicode_literals
 
+
 from django.db import models
 
 # Create your models here.
+
+class Press(models.Model):
+    title = models.CharField(max_length=100)
+    url = models.URLField()
+    source = models.CharField(max_length=50)
+    pubdate = models.DateField()
+
+    def __str__(self):
+        return(self.title + ' : ' + self.url + ' : ' + self.source + ' : ' + str(self.pubdate))
